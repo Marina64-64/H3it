6d6c9e9fb619e0bc8e3f6eb9a07c286099571b66
Marina <rinanashaat64@gmail.com>
2023-12-16 18:11:36 +0200

    add a sum function

--- main.cpp
+++ main.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
 using namespace std;
+int my_sum(int a, int b);
 int main()
 {
 	cout << "Welcome students" << endl;
 	cout << "Let's try new concepts" << endl;
+}
+int my_sum(int a, int b)
+{
+	return a + b;
 }
